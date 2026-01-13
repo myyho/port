335ad64eafd89d0dc0389550e25baaa34b4e03d2
myyho <gkthuongmyynhiu0102@gmail.com>
2026-01-13 11:24:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,415 @@
+
+function facebook() {
+    var url = "https://www.facebook.com/my.yy.241773/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function inl() {
+    var url = "https://www.linkedin.com/in/ý-hồ-hình-mỹ-2a80a027a"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function insta() {
+    var url = "https://www.instagram.com/ys_mymoni/?igsh=ejhqeHZwbWMwZ2s%3D&utm_source=qr"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function tiktok() {
+    var url = "https://www.tiktok.com/@florasion_macrame?_r=1&_d=secCgYIASAHKAESPgo8YMHSI%2BMd5p5H1namA63JFlGlzJiwpqvCi%2F56d8e%2FluIl2A2PI2xFoRPF228qD4tG5u3NQgPj%2B73h2R9pGgA%3D&_svg=1&checksum=a99f80f0bb70ffb7b60cc371e69b691aaa2a5241cf8a3fcbd2f965231bec8e86&item_author_type=1&sec_uid=MS4wLjABAAAAr7mI1CiIvN4NMmBVdM7idyGgVRad-2irnhyv8GNJJGjY-9-NJnr0-BH3jlw80cA5&sec_user_id=MS4wLjABAAAAr7mI1CiIvN4NMmBVdM7idyGgVRad-2irnhyv8GNJJGjY-9-NJnr0-BH3jlw80cA5&share_app_id=1180&share_author_id=7297938480004416513&share_link_id=6E6BA1A9-C726-4B47-BE3F-7D9B5FCAB6CF&share_region=VN&share_scene=1&sharer_language=vi&social_share_type=4&source=h5_t&timestamp=1767545431&tt_from=copy&u_code=eb08ke3g9e1i54&ug_btm=b8727%2Cb0&user_id=7297938480004416513&utm_campaign=client_share&utm_medium=ios&utm_source=copy"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+function btn_04_1() {
+    var url = ""; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function read_all_02() {
+    window.location.href ="21.html"; 
+}
+
+function btn_03_2(){
+    window.location.href ="certi.html"; 
+}
+
+function certi_01(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_327748a7e92e4106b1cca8cdd77ca94f.pdf"
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+// function certi_0(){
+//     var url=""
+//     // '_blank' là tham số bắt buộc để mở trong tab mới
+//     window.open(url, '_blank');
+// }
+
+
+
+function certi_02(){
+    var url=" https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_3fc15eaa2b4d4221ae021b836f890011.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+
+function certi_03(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_327748a7e92e4106b1cca8cdd77ca94f.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+function certi_04(){
+    var url="https://drive.google.com/file/d/1ksi2k10jz76S3O6c4MT8A8LSvuCq2fDP/view";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+
+function certi_05(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_56eb845daa7d41e19d176aa01036401b.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+
+function certi_06(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_58be9bcb1aa74267b0d7d69886844061.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+
+
+function certi_07(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_f80b89fb39eb4e699e6f894a8441caf3.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function certi_08(){
+    var url="https://2b92d4d5-638f-4eb7-b29b-190f822ff2fc.filesusr.com/ugd/e464f7_cd727f5550524062a9b3afb3e517745c.pdf";
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+function cvtec_page(){
+    window.location.href ="CVTEC.html"
+}
+
+function bsitf_page(){
+    window.location.href ="8.html"
+}
+
+function flo_page(){
+    window.location.href ="17.html"
+}
+
+
+function mm_page(){
+    window.location.href ="19.html"
+}
+
+
+function visef_page(){
+    window.location.href ="18.html"
+}
+
+function song_page(){
+    window.location.href ="20.html"
+
+}
+
+
+function testmonials(){
+    window.location.href ="21.html"
+}
+
+function btn_20_1(){
+    var url = "https://www.facebook.com/songproject1507"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+
+}
+
+function bsitf(){
+    var url = "https://www.facebook.com/clbsachbsitf"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+
+}
+
+function mo_trang_sach(){
+    window.location.href ="14.html"
+}
+
+
+function dieu_ky(){
+    window.location.href ="16.html"
+}
+
+function buoc_chan(){
+    window.location.href ="11.html"
+}
+
+function hanh_trinh_tri_thuc(){
+    window.location.href ="10.html"
+}
+
+function Q_A(){
+    window.location.href ="22.html"
+}
+
+function trang_tri_lop(){
+    window.location.href ="13.html"
+}
+
+function game_bat_chu(){
+    window.location.href ="15.html"
+}
+
+function tri_an(){
+    window.location.href ="12.html"
+}
+
+function gioi_thieu_sach(){
+    window.location.href ="9.html"
+}
+
+function gioi_thieu_fb(){
+    var url = "https://www.facebook.com/share/p/1DAfvabNEG/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function fb_hanh_trinh_tri_thuc(){
+    var url = "https://www.facebook.com/share/p/1BZX8NsSJ5/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function fb_buoc_chan_tuong_lai(){
+    var url = "https://www.facebook.com/share/p/1HRyx1aE1X/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function fb_tri_an(){
+    var url = "https://www.facebook.com/share/p/17y8KgZWJr/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+function fb_trang_tri_lop(){
+    var url = " https://www.facebook.com/share/p/1WkcrZWU83/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+function fb_mo_trang_sach(){
+    var url = " https://www.facebook.com/share/p/1aTZpeyf9a/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+function fb_bat_chu(){
+    var url = " https://www.facebook.com/share/p/1Cd9ST4DC2/"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function fb_dieu_ky(){
+    var url = " https://www.facebook.com/share/p/17oy2Phxe4/ "; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+function fb_flo(){
+    var url = "https://www.tiktok.com/@florasion_macrame?_r=1&_t=ZS-92vktAG2yub"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function visef_fb(){
+    var url = "https://drive.google.com/file/d/15GdotRNmQrHF9axDhLpuaUZk2cVjZPK3/view?usp=drive_link"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function btn_19_1(){
+    var url = "https://www.facebook.com/mastermind.hcmiu/about"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+function btn_19_2(){
+    var url = "https://myyho0308.wixsite.com/s-n-ph-m-ch--nha-kho"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function btn_19_3(){
+    var url = "https://tutam.ktoxz.id.vn/nha_chung"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+
+
+
+
+function flo() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function song() {
+    window.location.href ="https://www.facebook.com/songproject1507"; 
+}
+
+function sach() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+function MM() {
+    window.location.href ="https://www.facebook.com/my.yy.241773/"; 
+}
+
+
+function libary() {
+    window.location.href ="6.html"; 
+}
+
+function about() {
+    window.location.href ="4.html"; 
+}
+
+function contact() {
+    window.location.href ="23.html"; 
+    
+}
+
+function project(){
+    window.location.href ="6.html"; 
+}
+
+function home() {
+    window.location.href ="index.html"; 
+    
+}
+
+// ---------------------------------------------------------------------------------------------
+// cvtec
+function cvtecfb() {
+    var url = "https://www.facebook.com/cvtenglishclub"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+
+function mtm(){
+    var url = "https://www.facebook.com/cvtenglishclub/posts/pfbid0AnAfem51okQ2srBLked38Y9Aw4ZRu392KP16dJbWVKnNEJtcn3FRB669bemy1DtVl?rdid=8i2ILtEq1vTGcxeb#"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+function esc(){
+    var url = "https://www.facebook.com/cvtenglishclub/posts/pfbid02iHyf1mtCXopWbTLHbdRs9Rysv75M7uX5DjTY5pCRfnbXSBvf7svh8LM8CMdFaFJGl?rdid=1apCmkN5cyr5UiSd"; 
+    
+    // '_blank' là tham số bắt buộc để mở trong tab mới
+    window.open(url, '_blank');
+}
+
+const el1 = document.getElementById('el1');
+    const el2 = document.getElementById('el2');
+
+    // Khi chuột đi vào Element 1
+    el1.addEventListener('mouseenter', () => {
+      el2.classList.add('btn-animation');
+    });
+
+    // Khi chuột rời khỏi Element 1
+    el1.addEventListener('mouseleave', () => {
+      el2.classList.remove('btn-animation');
+    });
+    function submit() {
+        // Link Google Form (Giữ nguyên nếu bạn không tạo Form mới, chỉ sửa câu hỏi)
+        const formURL = 'https://docs.google.com/forms/d/e/1FAIpQLScbpLR7SStVGTbnRfNU0_np6diZN6wp_HSvEp2CkRd-hNNZyg/formResponse';
+    
+        // 1. Lấy dữ liệu (theo đoạn code bạn vừa gửi)
+        const name = document.getElementById("input_name").value;
+        const lname = document.getElementById("input_last_name").value; // Biến mới
+        const email = document.getElementById("input_email").value;
+        const phone = document.getElementById("input_phone").value;
+        const message = document.getElementById("input_message").value;
+    
+        // 2. Kiểm tra dữ liệu (Bắt buộc nhập Tên và Tin nhắn)
+        if (name === "" || message === "") {
+            alert("Vui lòng điền Tên và Nội dung tin nhắn!");
+            return;
+        }
+    
+        // 3. Đóng gói dữ liệu (Cần điền lại mã số mới)
+        const formData = new FormData();
+        
+        // --- LƯU Ý: Thay mã số bên dưới bằng mã bạn lấy được từ bước Get pre-filled link ---
+        formData.append("entry.865856870", name);     // Mã cho ô Tên
+        formData.append("entry.XBXBXBXBXB", lname);   // <--- THAY MÃ MỚI CHO Ô "HỌ" VÀO ĐÂY
+        formData.append("entry.1907687518", email);   // Mã cho ô Email
+        formData.append("entry.83723709", phone);     // Mã cho ô SĐT
+        formData.append("entry.777343319", message);  // Mã cho ô Tin nhắn
+    
+        // (Lưu ý: Mã entry của Subject cũ (2103816516) có thể không dùng được nữa nếu bạn đã xóa câu hỏi đó đi)
+    
+        // 4. Gửi đi
+        fetch(formURL, {
+            method: 'POST',
+            mode: 'no-cors',
+            body: formData
+        })
+        .then(() => {
+            alert("Cảm ơn " + name + " " + lname + "! Tin nhắn đã gửi thành công.");
+            
+            // Reset form
+            document.getElementById("input_name").value = "";
+            document.getElementById("input_last_name").value = "";
+            document.getElementById("input_email").value = "";
+            document.getElementById("input_phone").value = "";
+            document.getElementById("input_message").value = "";
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert("Có lỗi xảy ra!");
+        });
+    }
+
+
